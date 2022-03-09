4d83a7b79b710d9dc5b03b1a9552cdb3d41916ef
Jean <jeangarzon24@gmail.com>
2022-03-09 13:15:59 -0500

    API

--- LDigitalAPI/src/routes/index.js
+++ LDigitalAPI/src/routes/index.js
@@ -1,14 +1,9 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-// const registrarUsuario = require("./registroUsuario.js");
 const createPerson = require("./createPerson.js");
 const getPerson = require("./getPerson")
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-// router.use("/registrar", registrarUsuario);
+
 router.use("/person", createPerson);
 router.use("/person", getPerson);
 
